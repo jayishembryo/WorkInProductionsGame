a2ca8e2dae78766499d819fb97690ad77a246d64
BurnoutBeat <61676611+BurnoutBeat@users.noreply.github.com>
2024-09-03 19:45:59 -0500

    Did a decent chunk of some enemy code
    added the patrol, chase and attack functions.
    health is there and taking damage is also there.
    and added VSCode for my own sanity.

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,26 +3,136 @@
 //Changing this format later im lazy xd
 //This script tells an enemy to look at the player and move towards them at a set speed
 
+//Jake Gorski
+//Modifying 9/3/2024
+//Gunna mess up some code and see what happens :)
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
 
     private GameObject player;
+
+    private NavMeshAgent agent;
+
+    public float health;
     private float speed = 2f;
+    private LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    private float walkPointRange;
+
+    //Attacking
+    private float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    private float sightRange, attackRange;
+    private bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");  //Finds the player
+        agent = GetComponent<NavMeshAgent>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");  //Finds the player
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform);  //Makes the front of the object look towards the player, good for a 2d sprite in 3d space
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);  //Moves the enemy towards the player from the previous position by a set amount every frame
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        
+        if(!playerInSightRange && !playerInAttackRange) Patrolling();
+        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if(playerInSightRange && playerInAttackRange) AttackPlayer();
+        
+        
     }
 
+    private void Patrolling()
+    {
+        if(!walkPointSet) SearchWalkPoint();
+
+        if(walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange,walkPointRange);
+        float randomX = Random.Range(-walkPointRange,walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.transform.position); //the switch to chasing the player
+    }
+
+    private void AttackPlayer()//this is for a claw attack for when the player is up in their face or the other way around.
+    {
+        agent.SetDestination(transform.position); //make the enemy stop moving when attacking.
+
+        transform.LookAt(player.transform.position);
+        
+        if(!alreadyAttacked)
+        {   
+            //the attack code would go right here if I felt like it rn. Jake - 9/3/2024
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage)//enemy gets hit
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), 2f);
+        }
+    }
+
+    private void DestroyEnemy()//for adding flavor to a death
+    {
+        Destroy(gameObject);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
 }
