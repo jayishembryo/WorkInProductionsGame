e97624a49a6522518f071a5f1afaa47d2e2c2f42
Indigo102 <slitty@mail.bradley.edu>
2024-10-14 12:03:13 -0500

    Ambient Sounds Finished

--- Assets/Scripts/Sounds/AmbienceAudioManager.cs
+++ Assets/Scripts/Sounds/AmbienceAudioManager.cs
@@ -5,16 +5,27 @@ using FMOD.Studio;
 public class AmbienceAudioManager : MonoBehaviour
 {
     [SerializeField]
-    private EventReference ShipCreaking; 
+    private EventReference AmbienceSounds; 
 
     private EventInstance soundInstance;
 
     private void Awake()
     {
         // Create an FMOD Event Instance based on the provided event reference
-        soundInstance = RuntimeManager.CreateInstance(ShipCreaking);
+        soundInstance = RuntimeManager.CreateInstance(AmbienceSounds);
 
         PlaySoundLoop();
+    }
+
+    private void Update()
+    {
+        UpdateSoundPosition();
+    }
+
+    private void UpdateSoundPosition()
+    {
+        var attributes = RuntimeUtils.To3DAttributes(gameObject);
+        soundInstance.set3DAttributes(attributes);
     }
 
     private void PlaySoundLoop()
