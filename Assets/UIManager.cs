55ac926bfef3d9ddae1ebd8f4689347238c33920
Jacob Lee <jslee@mail.bradley.edu>
2024-10-17 03:30:36 -0500

    Animations In For Main Menu (missing functionality fixes, and deeper anims. also need new art)

---
+++ Assets/UIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SmoothShakePro;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Linkage")]
+    [SerializeField] private SmoothShakeManager sSM;
+    [SerializeField] private SmoothShake sSBoat;
+    [SerializeField] private SmoothShake sSBG;
+
+    [Header("Boat Tinkering")]
+    [Tooltip("Handles the Boat's rotational and positional amplification.")]
+    [SerializeField] private float boatAmp;
+
+    [Header("Background Tinkering")]
+    [Tooltip("Handles the Background's rotational and positional amplification.")]
+    [SerializeField] private float bgAmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Boat
+        sSBoat.SetShakerProperty(0, boatAmp, ShakerProperty.Amplitude);
+        sSBoat.SetShakerProperty(1, boatAmp, ShakerProperty.Amplitude);
+        sSM.StartShake("Wade");
+
+        // BG
+        sSBG.SetShakerProperty(0, bgAmp, ShakerProperty.Amplitude);
+        sSBG.SetShakerProperty(1, bgAmp, ShakerProperty.Amplitude);
+        sSM.StartShake("Tide");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
