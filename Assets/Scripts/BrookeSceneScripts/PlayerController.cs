44d4e8754b4d24713592911165ffbb9791ab3fea
PeachieDotZip <101476073+PeachieDotZip@users.noreply.github.com>
2024-10-19 00:15:58 -0500

    Boss Commit 3, almost done

--- Assets/Scripts/BrookeSceneScripts/PlayerController.cs
+++ Assets/Scripts/BrookeSceneScripts/PlayerController.cs
@@ -366,8 +366,8 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.layer == 13)
         {
 
-            FireScreen.SetActive(true);
-            BlazeEffect.SetActive(true);
+            if (FireScreen != null) { FireScreen.SetActive(false); }
+            if (BlazeEffect != null) { BlazeEffect.SetActive(false); }
 
             if (lastAddedToTime > whenToAddTime)
             {
@@ -386,8 +386,8 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.layer == 13)
         {
 
-            FireScreen.SetActive(false);
-            BlazeEffect.SetActive(false);
+            if (FireScreen != null) { FireScreen.SetActive(false); }
+            if (BlazeEffect != null) { BlazeEffect.SetActive(false); }
 
         }
     }
